7c9385ab8c4f315803720d88b73fdd05347ef162
Lasabi Olalekan <84998804+Larshabi@users.noreply.github.com>
2023-01-31 16:32:55 +0100

    uplaod

--- index.js
+++ index.js
@@ -7,14 +7,18 @@ const upload = require('./upload');
 const app = express();
 const server = http.Server(app);
 
-app.use(fileUpload());
+app.use(fileUpload({
+    useTempFiles: true,
+    limits: { fileSize: 50 * 1024 * 1024 }
+}));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/template', template.get);
-app.post('/', upload.post )
+app.post('/', upload.post);
+app.get('/report', upload.getReport)
 
 const options = {
     useNewUrlParser: true,
@@ -26,6 +30,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/csvimport', options)
         console.log(`Mongoose default connection to mongodb://127.0.0.1:27017/csvimport`)
     });;
 
-server.listen(8000, ()=>{
+server.listen(8000, () => {
     console.log('Server started on port 8000')
 })
--- upload.js
+++ upload.js
@@ -1,32 +1,55 @@
 const csv = require('fast-csv');
 const mongoose = require('mongoose');
 const Report = require('./model');
+const newcsv = require('csvtojson')
 
-exports.post = (req, res)=>{
-    if(!req.files){
+exports.post = async(req, res) => {
+    if (!req.files) {
         return res.status(400).json({
-            message:'No files was uploaded'
+            message: 'No files was uploaded'
         })
     }
     const reportFile = req.files.file;
-    const report = []
-    csv
-    .parseString(reportFile.data.toString(), {
-        headers: true,
-        ignoreEmpty: true
+    // console.log(req.files);
+    // csv
+    //     .parseString(reportFile.data.toString(), {
+    //         headers: true,
+    //         ignoreEmpty: true
+    //     })
+    //     .on("data", (data) => {
+    //         data['_id'] = new mongoose.Types.ObjectId();
+    //         console.log(data);
+    //         report.push(data);
+    //     })
+    //     .on("end", () => {
+    //         Report.create(report, (err, document) => {
+    //             if (err) {
+    //                 console.log(err);
+    //                 return res.status(400).json({
+    //                     error: err._message
+    //                 })
+    //             };
+    //         })
+    //         res.send(report.length + 'reports has been succefully uploaded')
+    //     })
+    const fd = await newcsv().fromFile(reportFile.tempFilePath);
+    // // fd.forEach((x) => {
+    // console.log(JSON.stringify(x).toString());
+    // })
+    console.log(fd)
+    let report;
+    fd.forEach(async(data) => {
+        report = await Report.create(data);
     })
-    .on("data",(data)=>{
-        data['_id'] = new mongoose.Types.ObjectId();
-        report.push(data);
-    })
-    .on("end", ()=>{
-        Report.create(report, (err, document)=>{
-            if(err) {
-                return res.status(400).json({
-                    error:err._message
-                })
-            };
-        })
-        res.send(report.length + 'reports has been succefully uploaded')
+    console.log('done');
+    return res.send(fd);
+
+}
+
+exports.getReport = async(req, res) => {
+    const report = await Report.find({});
+    return res.status(200).json({
+        report,
+        message: 'Done'
     })
 }
